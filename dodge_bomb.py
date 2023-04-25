a0a940cc3228f9233b3c56f40640ec3c65a728a8
tomoki <c0a2205445@edu.teu.ac.jp>
2023-04-25 15:51:14 +0900

    練習6

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -35,15 +35,15 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
     bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1
-    bb_img.set_colorkey((0, 0, 0))  #爆弾の四隅を透明にした
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #  練習1
+    bb_img.set_colorkey((0, 0, 0))  #  爆弾の四隅を透明にした
     x, y = random.randint(0, 1600), random.randint(0, 900)  # x,yを1600、900のランダムに設定
-    screen.blit(bb_img, [x, y])  # 練習2
+    screen.blit(bb_img, [x, y])  #  練習2
     vx, vy = +1, +1
-    bb_rct = bb_img.get_rect()  # Rectクラスに変更
-    bb_rct.center = x, y  # 初期位置をランダムに設定
-    kk_rct = kk_img.get_rect()  # Rectクラスに変更
-    kk_rct.center = 900, 400  #初期位置を900, 400に設定
+    bb_rct = bb_img.get_rect()  #  Rectクラスに変更
+    bb_rct.center = x, y  #  初期位置をランダムに設定
+    kk_rct = kk_img.get_rect()  #  Rectクラスに変更
+    kk_rct.center = 900, 400  #  初期位置を900, 400に設定
 
     while True:
         for event in pg.event.get():
@@ -51,7 +51,7 @@ def main():
                 return 0
 
         tmr += 1
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed()  #  
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
@@ -62,14 +62,17 @@ def main():
                     kk_rct.move_ip(-mv[0], -mv[1])
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)  # 練習4
-        bb_rct.move_ip(vx, vy)  # 練習３
+        screen.blit(kk_img, kk_rct)  #  練習4
+        bb_rct.move_ip(vx, vy)  #  練習３
         yoko, tate = check_bound(screen.get_rect(), bb_rct)
-        if not yoko:  # 横方向にはみ出たら
+        if not yoko:  #  横方向にはみ出たら
             vx*= -1
-        if not tate:
-            vy*= -1  # 縦方向にはみ出たら
-        screen.blit(bb_img, bb_rct)  # 練習３
+        if not tate:  #  縦方向にはみ出たら
+            vy*= -1
+        screen.blit(bb_img, bb_rct)  #  練習３
+
+        if kk_rct.colliderect(bb_rct):  #  こうかとん爆弾と当たったらmain関数からretrun する
+            return
 
         pg.display.update()
         clock.tick(1000)
